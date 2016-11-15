50c30e722efce0022e3c32cd3cd82eec8d239f09
Boris <rv.boris@yandex.ru>
2016-11-15 05:19:35 +0300

    feat: operation

--- src/shared/components/OperationEditForm/index.js
+++ src/shared/components/OperationEditForm/index.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { Card, CardHeader, CardBlock } from 'reactstrap';
-import { reduxForm, Field } from 'redux-form';
-import { get } from 'lodash';
+import { reduxForm, Field, formValueSelector } from 'redux-form';
+import { get, isUndefined } from 'lodash';
 import { injectIntl } from 'react-intl';
+import classnames from 'classnames';
+import TreeModel from 'tree-model';
+import moment from 'moment';
+import {
+  Card,
+  CardHeader,
+  CardBlock,
+  ButtonGroup,
+  Button,
+  Form,
+  FormGroup,
+  FormFeedback,
+  Label,
+  InputGroup,
+  InputGroupAddon,
+} from 'reactstrap';
 
 import config from '../../config';
 import { operationActions } from '../../actions';
 import DatePicker from '../DatePicker';
+import MoneyInput from '../MoneyInput';
+import SelectInput from '../SelectInput';
 import style from './style.css';
+
+const SelectFormField = field =>
+  <FormGroup color={field.meta.error ? 'danger' : null}>
+    <Label>{field.label}</Label>
+    <SelectInput
+      {...field}
+      options={field.options}
+      clearable={false}
+    />
+    {field.meta.touched && field.meta.error && <FormFeedback>{field.meta.error}</FormFeedback>}
+  </FormGroup>;
+
+const NumberFormField = field =>
+  <FormGroup color={field.meta.error ? 'danger' : null}>
+    <Label>{field.label}</Label>
+    <InputGroup>
+      <MoneyInput {...field} className="form-control" />
+      <InputGroupAddon>{field.currency.code}</InputGroupAddon>
+    </InputGroup>
+    {field.meta.touched && field.meta.error && <FormFeedback>{field.meta.error}</FormFeedback>}
+  </FormGroup>;
+
+const defaultValues = {
+  account: null,
+  accountFrom: null,
+  accountTo: null,
+  created: moment.utc().toString(),
+  category: null,
+  amount: null,
+};
+
+const fieldsToEdit = Object.keys(defaultValues);
 
 class OperationEditForm extends React.Component {
   static propTypes = {
@@ -22,28 +71,148 @@ class OperationEditForm extends React.Component {
     saveOperation: React.PropTypes.func.isRequired,
     removeOperation: React.PropTypes.func.isRequired,
     locale: React.PropTypes.string.isRequired,
+    mode: React.PropTypes.string.isRequired,
+    accountList: React.PropTypes.array.isRequired,
+    categoryList: React.PropTypes.array.isRequired,
+    selectedAccountCurrency: React.PropTypes.object,
   };
 
   constructor(props) {
     super(props);
 
-    this.setState({});
+    this.state = {
+      mode: props.mode,
+    };
+  }
+
+  getCategoriesList(mode) {
+    if (!['expense', 'income'].includes(mode)) {
+      return [];
+    }
+
+    const getNodeLabel = node => `${'- - '.repeat(node.getPath().length - 1)} ${node.model.name}`;
+
+    const categoryList = this.props.categoryList
+      .filter(node => node.model.type === node.model.type === 'any' || node.model.type === mode);
+
+    return categoryList.map(node => ({ value: node.model._id, label: getNodeLabel(node) }));
+  }
+
+  getAccountsList() {
+    return this.props.accountList.map(account => ({ value: account._id, label: account.name }));
+  }
+
+  toggleMode(mode) {
+    this.setState(Object.assign({}, this.state, { mode }));
   }
 
   render() {
     const { locale } = this.props;
+    const { mode } = this.state;
+
+    const categoriesList = this.getCategoriesList(mode);
+    const accountsList = this.getAccountsList();
 
     return (
       <Card>
         <CardHeader>Добавить операцию</CardHeader>
         <CardBlock>
-          <div className={style['datepicker-container']}>
-            <Field
-              name="date"
-              locale={locale}
-              component={DatePicker}
-            />
-          </div>
+          <Form className={style['content-container']}>
+            <div className={style['datepicker-container']}>
+              <Field
+                name="created"
+                locale={locale}
+                component={DatePicker}
+              />
+            </div>
+            <div className={classnames(style['form-container'], 'ml-1')}>
+              <ButtonGroup className="btn-group-justified mb-1">
+                <Button
+                  onClick={() => this.toggleMode('expense')}
+                  color="primary"
+                  type="button"
+                  outline
+                  active={mode === 'expense'}
+                >
+                  Расход
+                </Button>
+                <Button
+                  onClick={() => this.toggleMode('transfer')}
+                  color="primary"
+                  type="button"
+                  outline
+                  active={mode === 'transfer'}
+                >
+                  Перевод
+                </Button>
+                <Button
+                  onClick={() => this.toggleMode('income')}
+                  color="primary"
+                  type="button"
+                  outline
+                  active={mode === 'income'}
+                >
+                  Доход
+                </Button>
+              </ButtonGroup>
+
+              {['expense', 'income'].includes(mode) && [
+                <Field
+                  label="Категория"
+                  placeholder="Выбрать категорию"
+                  name="category"
+                  options={categoriesList}
+                  component={SelectFormField}
+                />,
+                <Field
+                  label="Счет"
+                  placeholder="Выбрать счет"
+                  name="account"
+                  options={accountsList}
+                  component={SelectFormField}
+                  disabled={accountsList.length === 1}
+                />,
+                <Field
+                  name="amount"
+                  label="Сумма"
+                  component={NumberFormField}
+                  currency={this.props.selectedAccountCurrency}
+                  type="number"
+                />,
+              ]}
+
+              {this.state.mode === 'transfer' && [
+                <Field
+                  label="Счет откуда"
+                  placeholder="Выбрать счет"
+                  name="accountFrom"
+                  options={accountsList}
+                  component={SelectFormField}
+                />,
+                <Field
+                  label="Счет куда"
+                  placeholder="Выбрать счет"
+                  name="accountTo"
+                  options={accountsList}
+                  component={SelectFormField}
+                />,
+                <Field
+                  name="amount"
+                  label="Сумма ушла"
+                  component={NumberFormField}
+                  currency={this.props.selectedAccountCurrency}
+                  type="number"
+                />,
+                <Field
+                  name="amount"
+                  label="Сумма пришла"
+                  component={NumberFormField}
+                  currency={this.props.selectedAccountCurrency}
+                  type="number"
+                />,
+              ]}
+            </div>
+          </Form>
         </CardBlock>
       </Card>
     );
@@ -62,14 +231,119 @@ const mapDispatchToProps = dispatch => ({
   removeOperation: (...args) => dispatch(operationActions.remove(...args)),
 });
 
+const formFieldSelector = formValueSelector('operationEdit');
+
+const processSelector = createSelector(
+  state => state.account.process,
+  process => process,
+);
+
 const localeSelector = createSelector(
   state => get(state, 'auth.profile.settings.locale', config.defaultLang),
   locale => locale,
 );
 
+const accountListSelector = createSelector(
+  state => state.account.accounts,
+  accountList => accountList
+);
+
+const operationSelector = createSelector(
+  (_, props) => props.operation,
+  operation => operation
+);
+
+const isNewOperationSelector = createSelector(
+  operationSelector,
+  operation => isUndefined(operation),
+);
+
+const operationDefaultsSelector = createSelector(
+  isNewOperationSelector,
+  operationSelector,
+  accountListSelector,
+  (isNewOperation, operation, accountList) => {
+    if (isNewOperation) {
+      if (accountList.length) {
+        defaultValues.account = accountList[0]._id;
+      }
+
+      return defaultValues;
+    }
+
+    return operation;
+  }
+);
+
+const initialModeSelector = createSelector(
+  isNewOperationSelector,
+  operationDefaultsSelector,
+  (isNewOperation, operation) => {
+    if (isNewOperation) {
+      return 'expense';
+    }
+
+    return operation.type;
+  }
+);
+
+const categoryTreeSelector = createSelector(
+  state => state.category.data,
+  categoryData => {
+    const tree = new TreeModel();
+    const rootNode = tree.parse(categoryData);
+
+    return rootNode;
+  }
+);
+
+const categoryListSelector = createSelector(
+  categoryTreeSelector,
+  categoryTree => categoryTree.all()
+);
+
+const selectedAccountCurrencySelector = createSelector(
+  operationDefaultsSelector,
+  state => formFieldSelector(state, ...fieldsToEdit),
+  state => state.currency.currencyList,
+  accountListSelector,
+  (initialValues, currentValues, currencyList, accountList) => {
+    const values = Object.assign({}, initialValues, currentValues);
+    const account = accountList.find(account => account._id === values.account);
+    const selectedCurrency = currencyList.find(currency => currency._id === account.currency);
+
+    return selectedCurrency;
+  },
+);
+
 const selector = createSelector(
   localeSelector,
-  locale => ({ locale })
+  processSelector,
+  accountListSelector,
+  categoryListSelector,
+  operationDefaultsSelector,
+  isNewOperationSelector,
+  initialModeSelector,
+  selectedAccountCurrencySelector,
+  (
+    locale,
+    process,
+    accountList,
+    categoryList,
+    initialValues,
+    isNewOperation,
+    mode,
+    selectedAccountCurrency
+  ) => ({
+    locale,
+    process,
+    accountList,
+    categoryList,
+    initialValues,
+    isNewOperation,
+    mode,
+    selectedAccountCurrency,
+  })
 );
 
 operationForm = connect(selector, mapDispatchToProps)(operationForm);
--- src/shared/containers/Operations/index.js
+++ src/shared/containers/Operations/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import { Alert } from 'reactstrap';
 
 import AccountList from '../../components/AccountList';
 import OperationEditForm from '../../components/OperationEditForm';
 import style from './style.css';
 
-const Operations = () => (
+const Operations = (props) => (
   <div className={style.operations}>
     <div className={classnames(style['operations-container'], 'mr-2')}>
-      <OperationEditForm />
+      { props.accountsExist && <OperationEditForm /> }
+      { !props.accountsExist && <Alert color="info">Счета не найдены</Alert> }
     </div>
     <div className={style['balance-container']}>
       <AccountList />
@@ -16,4 +20,18 @@ const Operations = () => (
   </div>
 );
 
-export default Operations;
+Operations.propTypes = {
+  accountsExist: React.PropTypes.bool.isRequired,
+};
+
+const accountsExistSelector = createSelector(
+  state => state.account.accounts,
+  accountList => accountList.length > 0
+);
+
+const selector = createSelector(
+  accountsExistSelector,
+  accountsExist => ({ accountsExist })
+);
+
+export default connect(selector)(Operations);
